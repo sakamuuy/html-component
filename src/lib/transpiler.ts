1a3a74c1452be3e2168698cefa2a576b4a1c88e0
sakamuuy <sakamuu.y@gmail.com>
2021-08-09 00:13:58 +0900

    nyaan

--- src/lib/transpiler.ts
+++ src/lib/transpiler.ts
@@ -1,11 +1,29 @@
 import fs from 'fs/promises'
+import * as esprima from 'esprima'
+import estraverse from 'estraverse'
+import { nyaan } from './types'
+
+const NYAAN_CALEE: nyaan.$NYAAN_COMPONENT = '$NYAAN_COMPONENT'
 
 async function readFile(path: string) {
   return await fs.readFile(path, 'utf-8')
 }
 
 function transpileJS(jsStr: string) {
-  console.log(jsStr)
+  const ast = esprima.parseScript(jsStr)
+  estraverse.traverse(ast, {
+    enter(node, parent) {
+      if (node.type === 'VariableDeclarator' && 
+        node.init?.type === 'CallExpression' &&
+        (node.init.callee as unknown as { type: string; name: string}).name === NYAAN_CALEE) {
+          
+        console.log('nyaan')
+      }
+    },
+    leave(node, parent) {
+
+    }
+  })
 }
 
 export async function transpile(path: string) {
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -10,4 +10,6 @@ export declare namespace nyaan {
     content: string[]
     isRoot: boolean
   }
+
+  type $NYAAN_COMPONENT = "$NYAAN_COMPONENT"
 }
