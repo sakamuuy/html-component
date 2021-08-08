c577f5df553ff3f0a879d985d2ff871656eb2df8
sakamuuy <sakamuu.y@gmail.com>
2021-08-08 20:52:45 +0900

    Fix parser

--- src/lib/domParser.ts
+++ src/lib/domParser.ts
@@ -1,6 +1,11 @@
 import fs from 'fs/promises'
-import { Parser } from 'htmlparser2'
+import * as htmlparser from 'htmlparser2'
 import { nyaan } from './types'
+
+type Wrapper = {
+  isClosed: boolean
+  el: nyaan.HTMLObj<keyof HTMLElementTagNameMap>
+}
 
 const createHTMLObj = <T extends keyof HTMLElementTagNameMap>(tagName: keyof HTMLElementTagNameMap, attributes: nyaan.HTMLObj<T>['attributes'], isRoot: boolean) => {
   const obj: nyaan.HTMLObj<T> = {
@@ -16,41 +21,57 @@ async function readHTML(path: string) {
   return await fs.readFile(path, 'utf-8')
 }
 
+// function findOpenParent(stack: Wrapper[]) {
+//   return 
+// }
+
 export async function parse(path: string) {
-  const htmlStr = await readHTML(path)
+  let root: Wrapper
+  let opening: Wrapper | null
+  const editingStack: Wrapper[] = []
 
-  let root: nyaan.HTMLObj<keyof HTMLElementTagNameMap>;
-  let opening: nyaan.HTMLObj<keyof HTMLElementTagNameMap> | null;
-
-  const parser = new Parser({
+  const parser = new htmlparser.Parser({
     onopentag(name: keyof HTMLElementTagNameMap, attributes) {
-      const obj = createHTMLObj<typeof name>(name, attributes, root? false : true)
+      const obj: Wrapper = {
+        isClosed: false,
+        el: createHTMLObj<typeof name>(name, attributes, root? false : true)
+      }
+
       if (!root) {
         root = obj
       }
 
       if (opening) {
-        opening.children.push(obj)
+        opening.el.children.push(obj.el)
       }
+
+      editingStack.push(obj)
 
       opening = obj
     },
     ontext(text) {
       if (opening) {
-        opening.children.push(text)
+        opening.el.children.push(text)
       }
     },
     onclosetag(tagname) {
-      if (opening?.tagName === tagname) {
-        opening = null
-      }  
+      if (opening?.el.tagName === tagname) {
+        opening.isClosed = true
+        editingStack.pop()
+        const openingParent = editingStack.reverse().find((obj) => !obj.isClosed)  
+        if (openingParent) {
+          opening = openingParent
+        }
+
+        // Fix order
+        editingStack.reverse()
+      }
     },
     onend() {
       console.log(JSON.stringify(root, null , "\t"))
     }
   })
 
-  parser.write(htmlStr)
+  parser.write((await readHTML(path)))
   parser.end()
 }
-
